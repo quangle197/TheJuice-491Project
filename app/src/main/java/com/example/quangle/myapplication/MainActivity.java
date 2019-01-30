d47ae9bf8001fe4db16dc57d193717bb39722923
Quang Le <quangle18997@gmail.com>
2019-01-29 23:10:48 -0800

    Fix menu java file

--- app/src/main/java/com/example/quangle/myapplication/MainActivity.java
+++ app/src/main/java/com/example/quangle/myapplication/MainActivity.java
@@ -80,13 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_message) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_order) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_selling) {
 
         }
         else if (id == R.id.nav_setting) {
