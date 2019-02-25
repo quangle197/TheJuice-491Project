dfe275f649a7b9c977679a8221b599dff5900399
Quang Le <quangle18997@gmail.com>
2019-02-25 00:37:36 -0800

    Made list item test

--- app/src/main/java/com/example/quangle/myapplication/UserInventoryActivity.java
+++ app/src/main/java/com/example/quangle/myapplication/UserInventoryActivity.java
@@ -1,12 +1,26 @@
 package com.example.quangle.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class UserInventoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup);
+        setContentView(R.layout.user_inventory);
+
+        Button signUp = (Button) findViewById(R.id.signUpButton);
+        signUp.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(v.getContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
